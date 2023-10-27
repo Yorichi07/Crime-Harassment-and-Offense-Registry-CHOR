94084825a7e337acb9e396d076dd39eab4e5ed74
Lakshit <Lakshitlucky11@gmail.com>
2023-10-27 21:47:45 +0530

    new

---
+++ FrontEnd/Login page/Login.js
@@ -0,0 +1,26 @@
+const subBtn = document.querySelector(".subBtn");
+const err = document.querySelector(".errMsg");
+
+subBtn.addEventListener("click",()=>{
+    const inp = document.querySelectorAll(".inpBx");
+
+    fetch("http://localhost:8080/api/LoginUser",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json; charset=UTF-8"
+        },
+        body:JSON.stringify({
+            "UserName":inp[0].value,
+            "PassWord":inp[1].value
+        })
+    }).then(res=>{
+        return res.json()
+    }).then(rep=>{
+        console.log(rep)
+        if(rep.ResCode==200){
+            window.alert(rep.Msg)
+        }else{
+            err.innerHTML = rep.Msg
+        }
+    })
+});
